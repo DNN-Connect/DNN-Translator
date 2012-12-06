d7fa9f6aef03121c2e27cb321656546f70bf4205
Peter Donker <peter@bring2mind.net>
2012-12-06 17:07:26 +0100

    Trying to fix column visibility not properly updating

---
+++ Common/BindingProxy.vb
@@ -0,0 +1,24 @@
+﻿Namespace Common
+ Public Class BindingProxy
+  Inherits Freezable
+#Region "Overrides of Freezable"
+
+  Protected Overrides Function CreateInstanceCore() As Freezable
+   Return New BindingProxy()
+  End Function
+
+#End Region
+
+  Public Property Data() As Object
+   Get
+    Return DirectCast(GetValue(DataProperty), Object)
+   End Get
+   Set(value As Object)
+    SetValue(DataProperty, value)
+   End Set
+  End Property
+
+  ' Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+  Public Shared ReadOnly DataProperty As DependencyProperty = DependencyProperty.Register("Data", GetType(Object), GetType(BindingProxy), New UIPropertyMetadata(Nothing))
+ End Class
+End Namespace
---
+++ Common/BoolToWidthConverter.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Globalization
+
+Namespace Common
+ Public Class BoolToWidthConverter
+  Implements IValueConverter
+  Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements System.Windows.Data.IValueConverter.Convert
+   Try
+    Dim v = CBool(value)
+    Return If(v, New GridLength(1, GridUnitType.Star), New GridLength(1))
+   Catch generatedExceptionName As InvalidCastException
+    Return Visibility.Collapsed
+   End Try
+  End Function
+
+  Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements System.Windows.Data.IValueConverter.ConvertBack
+   Throw New NotImplementedException()
+  End Function
+ End Class
+End Namespace
