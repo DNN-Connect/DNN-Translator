726a1f7f831a1b567d5227b88bbea5e8a44d7ee8
donker <peter@bring2mind.net>
2014-06-20 08:28:13 +0200

    Preparing release

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Windows
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("Translator")> 
-<Assembly: AssemblyCopyright("Copyright DotNetNuke Corp 2012")> 
+<Assembly: AssemblyCopyright("Copyright DotNetNuke Corp 2014")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: ComVisible(false)>
 
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.10.0")> 
-<Assembly: AssemblyFileVersion("1.0.10.0")> 
+<Assembly: AssemblyVersion("1.0.11.0")> 
+<Assembly: AssemblyFileVersion("1.0.11.0")> 
