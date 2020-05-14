da65c9d1e8ad32ce8eea0332b691c23c17a7d20c
Peter Donker <peter@bring2mind.net>
2020-05-14 17:23:21 +0200

    Bump version

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.3.0")>
-<Assembly: AssemblyFileVersion("1.1.3.0")>
+<Assembly: AssemblyVersion("1.2.1.0")>
+<Assembly: AssemblyFileVersion("1.2.1.0")>
